5d39977c59173f6514099921f821e42d9fb5f263
Ezekiel Hammond <ezekiel_hammond@dell.com>
2016-11-25 03:32:45 -0600

    Added Health System.

--- SublimeAdventures/Assets/Scripts/TextController.cs
+++ SublimeAdventures/Assets/Scripts/TextController.cs
@@ -13,7 +13,9 @@ public class TextController : MonoBehaviour {
 		
 	};
 	public static locations myLocation;
-
+	public static System.Random Attack = new System.Random();
+	public static int SwordBattle = Attack.Next(0, 100);
+	public static int EnemyAttack = Attack.Next(0, 100);
 
 	// Use this for initialization
 	void Start () {
@@ -67,7 +69,7 @@ public class TextController : MonoBehaviour {
 			myLocation = locations.room_0;
 		}
 
-
+		print(SwordBattle);
 
 	}
 
@@ -137,7 +139,7 @@ public class TextController : MonoBehaviour {
 	}
 
 	void corridor(){
-		text.text = "";
+		text.text = "When you exit the room you look around the corridor and see ";
 		}
 
 	void drawer_open(){
@@ -208,4 +210,9 @@ public class TextController : MonoBehaviour {
 			
 		}
 	}
+
+
+
+
+
 }
