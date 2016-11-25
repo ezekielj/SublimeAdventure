776e282d6f8c68156f6753abdaaa988754a3bf3c
Ezekiel Hammond <ezekiel_hammond@dell.com>
2016-11-25 03:33:40 -0600

    FUll Update

---
+++ SublimeAdventures/Assets/Scripts/health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class health : MonoBehaviour {
+
+	public Text myHealth;
+	public static string theHealth = "100";
+	// Use this for initialization
+	void Start () {
+		myHealth.text = theHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
